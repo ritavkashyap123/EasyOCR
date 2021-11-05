859e8d0ed71f37af1eb1f2a6f0d7aafb3f395218
Ritav Kashyap <79644483+ritavkashyap123@users.noreply.github.com>
2021-11-05 23:52:49 +0530

    successful

--- Easyocr.py
+++ Easyocr.py
@@ -1,9 +1,14 @@
 import easyocr
 import cv2
+from matplotlib import pyplot as plt
 import numpy as np
+import tensorflow as tf
 
-IMAGE_PATH = 'surf.png'
+IMAGE_PATH = 'payment.png'
 
 reader = easyocr.Reader(['en'])
-result = reader.readtext(IMAGE_PATH)
-result
+output = reader.readtext(IMAGE_PATH)
+
+for (bbox, text, prob) in output:
+    text = "".join ([c if ord(c)<128 else "" for c in text])
+    print (text)
